942665d7cb46225f83de2948f8be06bfa7876fd5
Asiak <asiakemp24@gmail.com>
2023-09-16 09:02:35 -0400

    made changes in auth.js and server.js files

--- server/server.js
+++ server/server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+// requiring more needed modules
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a instance of ApolloServer
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+// middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// add routes here if necessary
 app.use(routes);
 
 db.once('open', () => {
