12940772f368124bb18b6da85c9077f1ca118be0
Asiak <asiakemp24@gmail.com>
2023-09-18 18:54:13 -0400

    added logic in the resolvers.js file

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -12,6 +12,10 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+// import apollo client and save book mutation
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -26,6 +30,9 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  // initialize saveBook mutation
+  const [saveBookMutation] = useMutation(SAVE_BOOK);
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -63,27 +70,20 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
+    
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
+      // use save book mutation to save book
+      const { data } = await saveBookMutation({
+        variables: { input: bookToSave },
+      })
+      // // Handle the result of the mutation, e.g., displaying a success message
+      // and updating your component's state.
+      // data.saveBook will contain the result from the server.
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
-  };
+  }
 
   return (
     <>
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,16 +15,101 @@ const resolvers = {
 
     Mutation: {
         login: async (_, { email, password }) => {
-            // create login logic
+            try {
+                // find by email
+                const user = await User.findOne({ email});
+
+                // if no user found
+                if (!user || !user.isCorrectPassword(password)) {
+                    throw new AuthenticationError('Invalid user or password');
+                }
+                // if correct
+                const token = signToken(user);
+
+                return { token, user };
+            } catch (err) {
+                console.error(err);
+                throw new AuthenticationError('Something went wrong with the login');
+            }
         },
         addUser: async (_, { args }) => {
-            // create user registration logic
+            try {
+                // Check if a user with the provided email already exists
+                const existingUser = await User.findOne({ email });
+
+                // If a user with the email exists, throw an authentication error
+                if (existingUser) {
+                    throw new AuthenticationError('Email is already in use');
+                }
+
+                // Hash the password
+                const hashedPassword = await User.hashPassword(password);
+
+                // Create a new user record
+                const newUser = await User.create({
+                    username,
+                    email,
+                    password: hashedPassword,
+                });
+
+                // Sign a JWT token for the new user
+                const token = signToken(newUser);
+
+                // Return the token and user data
+                return { token, user: newUser };
+            } catch (err) {
+                console.error(err);
+                throw new AuthenticationError('Error creating a new user');
+            }
         },
         saveBook: async (_, { input }, context) => {
-            // logic to save book to users saved books
+            try {
+                // Check if the user is authenticated
+                if (!context.user) {
+                    throw new AuthenticationError('You need to be logged in to save books.');
+                }
+
+                // Find the user by their ID
+                const user = await User.findOne({ _id: context.user._id });
+
+                // Check if the book is already in the user's saved books
+                if (user.savedBooks.some((book) => book.bookId === input.bookId)) {
+                    return user; // Book is already saved, so return the user as-is
+                }
+
+                // Add the book to the user's saved books
+                user.savedBooks.push(input);
+
+                // Save the updated user document
+                await user.save();
+
+                return user; // Return the updated user data
+            } catch (err) {
+                console.error(err);
+                throw new AuthenticationError('Error saving the book.');
+            }
         },
         removeBook: async(_, { bookId }, context) => {
-            // logic to remove book from users saved books
+            try {
+                // Check if the user is authenticated
+                if (!context.user) {
+                    throw new AuthenticationError('You need to be logged in to remove books.');
+                }
+
+                // Find the user by their ID
+                const user = await User.findOne({ _id: context.user._id });
+
+                // Filter out the book with the specified bookId from savedBooks array
+                user.savedBooks = user.savedBooks.filter((book) => book.bookId !== bookId);
+
+                // Save the updated user document
+                await user.save();
+
+                return user; // Return the updated user data
+            } catch (err) {
+                console.error(err);
+                throw new AuthenticationError('Error removing the book.');
+            }
         },
     },
 };
